6c85e4d9cb805f9858632cc7c52eb9899fa838cb
mred5683 <hansenrd5683@gmail.com>
2016-12-03 00:09:52 -0800

    add fibonaccifun to repo

---
+++ FibonacciFun/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FibonacciFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables to store sequence
+            var a = 0;
+            var b = 1;
+            var c = 0;
+
+            //take input from user and store in variable
+            WriteLine("Enter a number");
+            var userInput = int.Parse(ReadLine());
+
+            //output for start of sequence
+            WriteLine(a);
+            WriteLine(b);
+
+            while (c <= userInput)
+            {
+                c = a + b;
+                if (c <= userInput)
+                {
+                    WriteLine(c);
+                }
+                else
+                {
+                    break;
+                }
+                a = b;
+                b = c;
+            }
+
+            a = 0;
+            b = 1;
+            c = 0;
+
+            while (c <= userInput)
+            {
+                c = a + b;
+
+                if (c == userInput)
+                {
+                    goto IsFib;
+                }
+
+                a = b;
+                b = c;
+            }
+            if (c != userInput)
+            {
+                goto IsNotFib;
+            }
+
+            IsFib:
+                WriteLine("True");
+                goto Finish;
+            IsNotFib:
+                WriteLine("False");
+            Finish:
+                WriteLine("Sequence Complete!!!");
+        }
+    }
+}
